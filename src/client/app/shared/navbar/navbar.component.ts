13f0835aff4824b045b4e58a372511bc09e2314f
pjaveri@nvidia.com <pjaveri@nvidia.com>
2017-01-09 15:22:28 -0800

    committing sidebar nav expandable code

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
-
+import {
+  Component,
+  Input,
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/core';
 /**
  * This class represents the navigation bar component.
  */
 @Component({
   moduleId: module.id,
   selector: 'sd-navbar',
-  templateUrl: 'navbar.component.html'
+  templateUrl: 'navbar.component.html',
+  animations: [
+    trigger('verticalOpen',[
+      state('inactive',style({
+        left: '-115px',
+        width:'50px'
+      })),
+      state('active',style({
+        left: '0px',
+        width:'250px'
+      })),
+      transition('active => inactive', animate('100ms ease-in')),
+      transition('inactive => active', animate('100ms ease-out'))
+    ])
+  ]
 })
 
-export class NavbarComponent { }
+export class NavbarComponent {
+
+  private hideSideNav: boolean = false;
+  private closeOrOpen:string;
+
+  toggleState() {
+
+    if (this.hideSideNav) {
+      this.hideSideNav = false;
+      this.closeOrOpen='inactive'
+    }
+    else {
+      this.hideSideNav = true;
+      this.closeOrOpen='active'
+    }
+
+    console.log( this.hideSideNav);
+  }
+
+}
